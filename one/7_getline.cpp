d516a5c0279b1a9a640e7c88693cc636858a2ccc
Pranav C R <pranavcr001@gmail.com>
2025-10-24 00:18:09 +0530

    getline, char

--- one/7_getline.cpp
+++ one/7_getline.cpp
@@ -2,15 +2,12 @@
 using namespace std;  
 
 int main(){
-    // string 
-    string s;
-    cin >> s;
-    cout << s;  // THIS WILL TAKE ONLY FIRST WORD OF INPUT. IT STOPS AT SPACE.
+    // getline
 
-    string s1,s2;   // string s1;
-                    // string s2;
-    cin >> s1 >> s2;     // first word in s1, second word in s2.
-    cout << s1 << " " << s2;
+    string str;
+    getline(cin,str);
+    cout << str;   // prints the entire line (upto the first newline character)
+                   // stops at line break
         
     return 0;
 }
---
+++ one/8_char.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>   
+using namespace std;  
+
+int main(){
+    // char
+    char ch = 'H';
+    cout << ch;
+
+    char cha;
+    cin >> cha;
+    cout << cha;
+
+    string chi = "H";
+    cout << chi;
+
+    // generally chars we use single quote
+    // strings double quote
+
+    return 0;
+}
