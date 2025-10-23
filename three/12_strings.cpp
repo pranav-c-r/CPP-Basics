51195d0fbd6c0dd6b5234e2b57890cfdc6926eee
Pranav C R <pranavcr001@gmail.com>
2025-10-24 02:30:46 +0530

    Create 12_strings.cpp

---
+++ three/12_strings.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s = "Striver";
+    int len = s.size();
+    s[len-1] = 'z';
+    cout << s[len-1];  //output : z
+    return 0;
+}
